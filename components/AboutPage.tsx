f6a8728d6a92ab9223d6ddf1983484bbfccba225
Zack Steinkamp <zack@steinkamp.us>
2024-03-09 22:14:50 -0800

    uggh lame

--- components/AboutPage.tsx
+++ components/AboutPage.tsx
@@ -8,13 +8,13 @@ export default function AboutPage() {
         One of my hobbies is music-making with the computer in Ableton Live. Part of the workflow is to render songs as .wav files either to listen elsewhere or to submit for publishing to services like Apple Music or Spotify.
       </p>
       <p className="mb-4">
-        In order to listen elsewhere, normally you would need to copy the .wav file to your phone, or upload it to a service like SoundCloud or Bandcamp. This works, but it's an extra step and extra steps are a drag!
+        In order to listen elsewhere, normally you would need to copy the .wav file to your phone, or upload it to a service like SoundCloud or Bandcamp. This works, but it&apos;s an extra step and extra steps are a drag!
       </p>
       <p className="mb-4">
         I wrote `musics` to serve my .wav file export directory over the Internet, mostly so that I could listen to works-in-progress in the car, in different headphones, on bluetooth speakers, etc.
       </p>
       <p className="mb-4">
-        When I'm working on a song, I might export 30 or more versions before I'm satisfied. I have a naming convention for the versions (name-00.wav, name-01.wav, ...), and `musics` embraces that. It will play the newest version by default, but you can also A/B test different versions in the browser.
+        When I&apos;m working on a song, I might export 30 or more versions before I&apos;m satisfied. I have a naming convention for the versions (name-00.wav, name-01.wav, ...), and `musics` embraces that. It will play the newest version by default, but you can also A/B test different versions in the browser.
       </p>
       <h2 className="text-2xl mb-4 font-header text-headertext-light dark:text-headertext-dark">
         Contributing
